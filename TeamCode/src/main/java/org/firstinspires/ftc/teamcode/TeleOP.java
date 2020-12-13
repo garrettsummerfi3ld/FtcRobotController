b87001b71b4f5449f1b59380ee34658c8fd5d71e
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2020-12-13 17:55:01 -0600

    Initial setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "TeleOp", group = "OakvilleDynamicsTest")
+public class TeleOP extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
